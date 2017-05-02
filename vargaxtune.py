1830885c91c97ac2fc79d0503ebc65b24aef4938
Miguel Vargas <miguelchico14@gmail.com>
2017-05-02 13:07:00 +0200

    primero pasos d

--- vargaxtune.py
+++ vargaxtune.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
-from bottle import route, run, request, redirect, template, static_file
-import json
-import requests
+from bottle import route, run, request, template, static_file
 
-@route('/inicio')
+@route('/inicio',method="get")
 @route('/')
 def inicio():
-    return """<h1>Hola
+    return template('html/inicio.tpl')
+
+@route('/inicio',method="post")
+def inicio2():
+    para1=str(request.forms.get('nombre'))
+    para2=str(request.forms.get('tipo'))
+    return """<h1>%s %s</h1>"""%(para1,para2)
+
+@route('/file /<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath,root='html/style')
+
+run(host='0.0.0.0', port=8081, debug=True)
