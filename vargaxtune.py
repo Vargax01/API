d4e2eb8820542ac1bf637c5693246e80af40e191
Miguel Vargas <miguelchico14@gmail.com>
2017-05-18 12:51:14 +0200

    Intentado Twitterar

--- vargaxtune.py
+++ vargaxtune.py
@@ -1,11 +1,44 @@
 # -*- coding: utf-8 -*-
 from bottle import route, run, request, template, static_file, redirect
 import requests
+from requests_oauthlib import OAuth1
+from urlparse import parse_qs
 from sys import argv
 import json
 import sendgrid
 import os
 from sendgrid.helpers.mail import *
+
+REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
+AUTHENTICATE_URL = "https://api.twitter.com/oauth/authenticate?oauth_token="
+ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
+
+CONSUMER_KEY = "5lWOSvU6M66CrZTkOGTZJ0W8B"
+CONSUMER_SECRET = "jHgZn8qW6QFJXYCjFZcX2fIsVkdkjl1NExRfKGX3mtrHNHqKE5"
+
+TOKENS = {}
+
+def get_request_token():
+    oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+    )
+    r = requests.post(url=REQUEST_TOKEN_URL, auth=oauth)
+    credentials = parse_qs(r.content)
+    TOKENS["request_token"] = credentials.get('oauth_token')[0]
+    TOKENS["request_token_secret"] = credentials.get('oauth_token_secret')[0]
+
+def get_access_token(TOKENS):
+  oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+                   resource_owner_key=TOKENS["request_token"],
+                   resource_owner_secret=TOKENS["request_token_secret"],
+                   verifier=TOKENS["verifier"],)
+  r = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
+  credentials = parse_qs(r.content)
+  TOKENS["access_token"] = credentials.get('oauth_token')[0]
+  TOKENS["access_token_secret"] = credentials.get('oauth_token_secret')[0]
+
+
 @route('/inicio',method="get")
 @route('/')
 def inicio():
@@ -53,7 +86,11 @@ def canciones(codigo):
 
 @route('/correo/<codigocan>',method="get")
 def correo(codigocan):
-    return template('html/correo.tpl',codigocan=codigocan)
+    get_request_token()
+    authorize_url = AUTHENTICATE_URL + TOKENS["request_token"]
+    response.set_cookie("request_token", TOKENS["request_token"],secret='some-secret-key')
+    response.set_cookie("request_token_secret", TOKENS["request_token_secret"],secret='some-secret-key')
+    return template('html/correo.tpl',codigocan=codigocan,authorize_url=authorize_url)
 
 @route('/correo/<codigocan>',method="post")
 def correo2(codigocan):
@@ -65,7 +102,7 @@ def correo2(codigocan):
     album=js["results"][0]["collectionName"]
     artista=js["results"][0]["artistName"]
     imagen=js["results"][0]["artworkUrl100"]
-    sg = sendgrid.SendGridAPIClient(apikey='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')
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ('SENDGRID_API_KEY')
     from_email = Email("miguelchico14@gmail.com")
     subject = "Cancion que me ha gustado desde VargaxTune"
     to_email = Email(correo)
@@ -73,6 +110,46 @@ def correo2(codigocan):
     mail = Mail(from_email, subject, to_email, content)
     response = sg.client.mail.send.post(request_body=mail.get())
     return template('html/correoenviado.tpl')
+
+@get('/callback')
+def get_verifier():
+  TOKENS["request_token"]=request.get_cookie("request_token", secret='some-secret-key')
+  TOKENS["request_token_secret"]=request.get_cookie("request_token_secret", secret='some-secret-key')
+  TOKENS["verifier"] = request.query.oauth_verifier
+  get_access_token(TOKENS)
+  response.set_cookie("access_token", TOKENS["access_token"],secret='some-secret-key')
+  response.set_cookie("access_token_secret", TOKENS["access_token_secret"],secret='some-secret-key')
+  redirect('/twittear')
+
+@get('/twittear')
+def twittear():
+    if request.get_cookie("access_token", secret='some-secret-key'):
+      TOKENS["access_token"]=request.get_cookie("access_token", secret='some-secret-key')
+      TOKENS["access_token_secret"]=request.get_cookie("access_token_secret", secret='some-secret-key')
+      print CONSUMER_KEY
+      print CONSUMER_SECRET
+      print TOKENS["access_token"]
+      print TOKENS["access_token_secret"]
+      oauth = OAuth1(CONSUMER_KEY,
+                       client_secret=CONSUMER_SECRET,
+                       resource_owner_key=TOKENS["access_token"],
+                       resource_owner_secret=TOKENS["access_token_secret"])
+      url = 'https://api.twitter.com/1.1/statuses/update.json'
+      r = requests.post(url=url,
+                          data={"status":"prueba de oauth1"},
+                          auth=oauth)
+      if r.status_code == 200:
+        return "<p>Tweet properly sent</p>"
+      else:
+        return "<p>Unable to send tweet</p>"
+    else:
+      redirect('/inicio')
+
+@get('/twitter_logout')
+def twitter_logout():
+  response.set_cookie("access_token", '',max_age=0)
+  response.set_cookie("access_token_secret", '',max_age=0)
+  redirect('/inicio')
 
 @route('/artista/<para1>')
 def artista(para1):
@@ -90,5 +167,5 @@ def error():
 def server_static(filepath):
     return static_file(filepath,root='html/style')
 
-#run(host='0.0.0.0', port=argv[1])
-run(host='0.0.0.0', port=8081, debug=True)
+run(host='0.0.0.0', port=argv[1])
+#run(host='0.0.0.0', port=8081, debug=True)
